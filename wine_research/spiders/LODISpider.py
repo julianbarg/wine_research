a90b23254c2426e12223bcc74d9edd63743b2c93
julianbarg <uoeieH12>
2019-02-21 12:04:41 -0500

    Fixed bugs for live website scraping.

--- wine_research/spiders/LODISpider.py
+++ wine_research/spiders/LODISpider.py
@@ -19,58 +19,103 @@ class LodiSpider(Spider):
         # Handle archived version of website.
         if 'archive.org' in response.url:
             organizations = response.xpath('//*[@class="entry"]//td')
+            date = datetime.strptime(response.url[28:36], '%Y%m%d').strftime('%Y-%m-%d')
+
+            for organization in organizations:
+                info = organization.xpath('.//text()').extract()
+                info = list(map(str.strip, info))
+
+                # Check first address format.
+                if len(info) > 2 and any(term in info[2] for term in ('CA', 'California')):
+                    loader = ItemLoader(item=LODIItem(), selector=organization)
+                    loader.add_value('date', date)
+
+                    name = info[0]
+                    loader.add_value('name', name)
+
+                    address = '\n'.join(info[1:3])
+                    loader.add_value('address', address)
+
+                    if len(info) > 3:
+                        phone = info[3]
+                        loader.add_value('phone', phone)
+
+                    if len(info) > 4:
+                        contact = info[4]
+                        loader.add_value('contact', contact)
+
+                    yield loader.load_item()
+
+                # Check second address format.
+                elif len(info) > 1 and any(term in info[1] for term in ('CA', 'California')):
+                    loader = ItemLoader(item=LODIItem(), selector=organization)
+                    loader.add_value('date', date)
+
+                    name = info[0]
+                    loader.add_value('name', name)
+
+                    address = info[1]
+                    loader.add_value('address', address)
+
+                    if len(info) > 2:
+                        phone = info[2]
+                        loader.add_value('phone', phone)
+
+                        contact = info[3]
+                        loader.add_value('contact', contact)
+
+                    yield loader.load_item()
+
         # Handle live website.
         else:
-            organizations = response.xpath('//*[@class="v65-col3 col3-certvineyard"]')
+            organizations = response.xpath('//p')
+            date = datetime.now().strftime('%Y-%m-%d')
 
-        for organization in organizations:
-            info = organization.xpath('.//text()').extract()
-            info = list(map(str.strip, info))
-            # info = [line.replace('\n', '') for line in info]
-            # Check first address format.
-            if len(info) > 2 and any(term in info[2] for term in ('CA', 'California')):
-                loader = ItemLoader(item=LODIItem(), selector=organization)
+            for organization in organizations:
+                info = organization.xpath('.//text()').extract()
+                info = list(map(str.strip, info))
 
-                name = info[0]
-                loader.add_value('name', name)
+                # Check first address format.
+                if len(info) > 3 and any(term in info[3] for term in ('CA', 'California')):
+                    loader = ItemLoader(item=LODIItem(), selector=organization)
+                    loader.add_value('date', date)
 
-                date = datetime.strptime(response.url[28:36], '%Y%m%d').strftime('%Y-%m-%d')
-                loader.add_value('date', date)
+                    name = info[1]
+                    loader.add_value('name', name)
 
-                address = '\n'.join(info[1:3])
-                loader.add_value('address', address)
+                    address = '\n'.join(info[2:4])
+                    loader.add_value('address', address)
 
-                if len(info) > 3:
-                    phone = info[3]
-                    loader.add_value('phone', phone)
+                    if len(info) > 4:
+                        phone = info[4]
+                        loader.add_value('phone', phone)
 
-                if len(info) > 4:
-                    contact = info[4]
-                    loader.add_value('contact', contact)
+                    if len(info) > 6:
+                        contact = info[6]
+                        loader.add_value('contact', contact)
 
-                yield loader.load_item()
+                    yield loader.load_item()
 
-            # Check second address format.
-            elif len(info) > 1 and any(term in info[1] for term in ('CA', 'California')):
-                loader = ItemLoader(item=LODIItem(), selector=organization)
+                # Check second address format.
+                elif len(info) > 2 and any(term in info[2] for term in ('CA', 'California')):
+                    loader = ItemLoader(item=LODIItem(), selector=organization)
+                    loader.add_value('date', date)
 
-                name = info[0]
-                loader.add_value('name', name)
+                    name = info[1]
+                    loader.add_value('name', name)
 
-                date = datetime.strptime(response.url[28:36], '%Y%m%d').strftime('%Y-%m-%d')
-                loader.add_value('date', date)
+                    address = info[2]
+                    loader.add_value('address', address)
 
-                address = info[1]
-                loader.add_value('address', address)
+                    if len(info) > 3:
+                        phone = info[3]
+                        loader.add_value('phone', phone)
 
-                if len(info) > 2:
-                    phone = info[2]
-                    loader.add_value('phone', phone)
+                    if len(info) > 5:
+                        contact = info[5]
+                        loader.add_value('contact', contact)
 
-                    contact = info[3]
-                    loader.add_value('contact', contact)
-
-                yield loader.load_item()
+                    yield loader.load_item()
 
         next_page = response.xpath('//*[@class="f"]/a/@href').extract_first()
         if next_page:
